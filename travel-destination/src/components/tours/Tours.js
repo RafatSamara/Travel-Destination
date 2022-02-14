81e6ef33c3f963f4976750df5608cac7478dcbab
Rafat Samara <rafat.samara@hotmail.co.uk>
2022-02-14 08:37:32 +0200

    Solved

---
+++ travel-destination/src/components/tours/Tours.js
@@ -0,0 +1,27 @@
+import data from "../../data/db.json";
+import classes from "./Tours.css";
+
+export default function Tours() {
+  let travelDestinations = data.map((props) => {
+    return (
+
+      <tr key={props.id}>
+        <td><h5>{props.id}</h5></td>
+        <td><img id="img" src={props.image} alt={"Trip Image"} /></td>
+        <td><h1>{props.name}</h1></td>
+        <td><h3>{props.info}</h3></td>
+        <td><h1>{props.price}</h1></td>
+      </tr>
+    );
+  });
+  return <table>
+    <tr>
+      <th>Trip ID</th>
+      <th>Image</th>
+      <th>Destination</th>
+      <th>More Info</th>
+      <th>Price / USD</th>
+    </tr>
+    {travelDestinations}
+  </table>;
+};
